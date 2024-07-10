31d7eaf268626bce725f7a6b129491082d13cb0b
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-07-10 18:57:59 -0300

    updatePorfolio

--- src/components/Main/Project.jsx
+++ src/components/Main/Project.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import '../../pages/Project/ProjectPage.css'; 
+import JavaScript from '../Icons/JavaScript';
+import ReactJS from '../Icons/React';
+import MongoDB from '../Icons/Mongo';
 
 const TAGS = {
   REACT: {
     name: "React",
     class: "bg-blue text-white",
-    icon: () => <span>🔵</span>, // Placeholder icon, replace with an actual icon if needed
+    icon: () => <ReactJS className="project-tag-icon" />,
   },
   JAVASCRIPT: {
     name: "JavaScript",
     class: "bg-yellow text-black",
-    icon: () => <span>💛</span>, // Placeholder icon, replace with an actual icon if needed
+    icon: () => <JavaScript className="project-tag-icon" />,
+  },
+  HTML5: {
+    name: "HTML5",
+    class: "bg-orange text-white",
+    icon: () => (
+      <svg className="project-tag-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 452 520">
+        <path fill="#e34f26" d="M41 460L0 0h451l-41 460-185 52" />
+        <path fill="#ef652a" d="M226 472l149-41 35-394H226" />
+        <path fill="#ecedee" d="M226 208h-75l-5-58h80V94H84l15 171h127zm0 147l-64-17-4-45h-56l7 89 117 32z"/>
+        <path fill="#fff" d="M226 265h69l-7 73-62 17v59l115-32 16-174H226zm0-171v56h136l5-56z"/>
+      </svg>
+    ),   
+  },
+  CSS: {
+    name: "CSS",
+    class: "bg-blue text-white",
+    icon: () => (
+      <svg className="project-tag-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 452 520">
+        <path fill="#0c73b8" d="M41 460L0 0h451l-41 460-185 52"/>
+        <path fill="#30a9dc" d="M226 472l149-41 35-394H226"/>
+        <path fill="#ecedee" d="M226 208H94l5 57h127zm0-114H84l5 56h137zm0 261l-124-33 7 60 117 32z"/>
+        <path fill="#fff" d="M226 265h69l-7 73-62 17v59l115-32 26-288H226v56h80l-6 58h-74z"/>
+      </svg>
+    ),
+  },
+  MONGODB: {
+    name: "MONGODB",
+    class: "bg-green text-white",
+    icon: () => <MongoDB className="project-tag-icon" />,
   },
 };
 
@@ -18,18 +50,26 @@ const PROJECTS = [
   {
     title: "React Weather App",
     description: "A weather forecasting app built with React. Provides real-time weather updates and forecasts for any location. Integrated with OpenWeatherMap API.",
-    link: "https://reactweatherapp.com",
-    github: "https://github.com/user/react-weather-app",
-    image: "https://via.placeholder.com/70", // Placeholder image
-    tags: [TAGS.REACT, TAGS.JAVASCRIPT],
+    link: "",
+    github: "",
+    image: "./src/img/project/FormulawebProject.png",
+    tags: [TAGS.HTML5, TAGS.CSS],
   },
   {
     title: "JS Game Collection",
     description: "A collection of simple but fun games built with JavaScript. Includes classic games like Snake, Tetris, and Pong.",
-    link: "https://jsgamecollection.com",
-    github: "https://github.com/user/js-game-collection",
-    image: "https://via.placeholder.com/70", // Placeholder image
+    link: "",
+    github: "",
+    image: "./src/img/project/ProgrammingProject.png",
     tags: [TAGS.JAVASCRIPT],
+  },
+  {
+    title: "JS Game Collection",
+    description: "A collection of simple but fun games built with JavaScript. Includes classic games like Snake, Tetris, and Pong.",
+    link: "",
+    github: "",
+    image: "./src/img/project/E-ecommersBackend.png",
+    tags: [TAGS.REACT, TAGS.JAVASCRIPT, TAGS.MONGODB],
   },
 ];
 
@@ -48,7 +88,7 @@ const Projects = () => (
               {tags.map((tag) => (
                 <li key={tag.name}>
                   <span className={`project-tag ${tag.class}`}>
-                    <tag.icon className="project-tag-icon" />
+                    {tag.icon()}
                     {tag.name}
                   </span>
                 </li>
