83d039e1af083943436438416e1b8b2bfb6d28d6
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-08-07 00:14:38 -0300

    update info

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -10,6 +10,7 @@ import DW from '../../img/certificated/649f245b4268360002c60e46.png'
 import JS from '../../img/certificated/64f09acf8278ce9cace9ede1.png'
 import RJS from '../../img/certificated/65548694b4768ce51454cd11.png'
 import PB from '../../img/certificated/663b8342bccd557071499eb8.png'
+import Accordion from '../../pages/Service/Accordion'
 
 const AboutPage = () => {
   function readMore() {
@@ -49,21 +50,14 @@ const AboutPage = () => {
             </div>
             <div className="sobre-mi-info">
               <p>
+              Hola, mi nombre el Mijael Figueroa ,tengo 20 años y estudio Desarrollo Web en la Universidad Pública de Ezeiza (UPE). He completado un año y medio de formación en Coderhouse, donde me certifiqué en Desarrollo Web, JavaScript, React JS y Backend, dominando tanto el front-end como el back-end. Actualmente, estoy realizando un curso de Ciberseguridad en Coderhouse para mejorar mis habilidades en ese campo.
+              Aunque no tengo experiencia laboral en programación, confío en mis habilidades, inteligencia, capacidad de resolución de problemas y rápida absorción de conocimientos para aportar significativamente a cualquier empresa o proyecto. Me gusta estar en constante aprendizaje, conociendo nuevas herramientas y técnicas para ser más eficaz en el trabajo.
+              Disfruto trabajando en equipo y colaborando con otros para alcanzar objetivos comunes. Mi objetivo es convertirme en un gran programador, dominando varios campos y contribuyendo ampliamente a un equipo de trabajo donde se logren grandes avances. Aspiro a crecer continuamente en el apasionante mundo de la programación y el desarrollo web.
               </p>
-
-              <div className="hide parrafo-active">
-                <p>
-                </p>
-
-                <p>
-                </p>
-              </div>
-
               <div className="btn-info">
                 <div className="custom-btn btn-codigo" id="btn-hide" onClick={readMore}><span>Read more</span></div>
               </div>
             </div>
-
           </div>
 
           <div className="skill-seccion">
@@ -73,30 +67,28 @@ const AboutPage = () => {
                 <img alt="HTML" className="skills-img icon-li" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" title="HTML5" />
                 <h2 className="skill-name">HTML</h2>
                 <p className="skill-info">
-                </p>
+                  Poseo una sólida comprensión de los fundamentos de HTML. Mis conocimientos en HTML me permiten crear estructuras web sólidas y bien organizadas, sirviendo como una base eficaz para la integración con CSS y JavaScript.
+                </p> 
               </div>
               <div className="skill-card" data-aos="flip-up" data-aos-delay="300">
                 <img alt="CSS" className="skills-img icon-li" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" title="CSS3" />
                 <h2 className="skill-name">CSS</h2>
                 <p className="skill-info">
+                Tengo experiencia en CSS, especializado en diseño responsivo y creación de animaciones suaves con AOS. Manejo la integración de estilos en proyectos React, utilizando preprocesadores como Sass y aprovechando las ventajas de Bootstrap para un desarrollo eficiente y profesional.
                 </p>
               </div>
               <div className="skill-card" data-aos="flip-right" data-aos-delay="300">
                 <img alt="Js" className="skills-img icon-li" src="https://cdn.iconscout.com/icon/free/png-256/javascript-2752148-2284965.png" title="Java Script" />
                 <h2 className="skill-name">Javascript</h2>
                 <p className="skill-info">
-                </p>
-              </div>
-              <div className="skill-card" data-aos="flip-left" data-aos-delay="300">
-                <img alt="Sass" className="skills-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" title="Sass" />
-                <h2 className="skill-name">Sass</h2>
-                <p className="skill-info">
+                Poseo experiencia en el desarrollo de aplicaciones utilizando JavaScript. Creo funciones dinámicas, manipulo el DOM eficientemente y gestiono eventos para construir interfaces interactivas. Además, manejo la integración con APIs RESTful para la obtención y envío de datos, y optimizo el rendimiento del código mediante buenas prácticas y técnicas avanzadas de programación.
                 </p>
               </div>
               <div className="skill-card" data-aos="flip-down" data-aos-delay="300">
                 <img alt="Bootstrap" className="skills-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" title="Bootstrap" />
                 <h2 className="skill-name">Bootstrap</h2>
                 <p className="skill-info">
+                Tengo conocimientos en utilizar Bootstrap para crear interfaces responsivas y modernas, aprovechando sus componentes y clases predefinidas para un desarrollo rápido y eficiente.
                 </p>
               </div>
               
@@ -104,18 +96,21 @@ const AboutPage = () => {
                 <img alt="react" className="skills-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" title="jQuery" />
                 <h2 className="skill-name">ReactJs</h2>
                 <p className="skill-info">
+                Tengo experiencia en el desarrollo de aplicaciones con React.js, creando componentes reutilizables y gestionando estados para construir interfaces de usuario dinámicas y eficientes. Además, utilizo Vite para optimizar el rendimiento de las aplicaciones.
                 </p>
               </div>
               <div className="skill-card" data-aos="flip-up" data-aos-delay="300">
                 <img alt="nodejs" className="skills-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" title="jQuery" />
                 <h2 className="skill-name">NodeJs</h2>
                 <p className="skill-info">
+                Tengo experiencia en el desarrollo de aplicaciones backend utilizando Node.js. He creado servidores eficientes y manejado operaciones asincrónicas, promesas async/await. Utilizo módulos modernos para el manejo avanzado de archivos. Implemento patrones de diseño modernos, como microservicios y arquitecturas basadas en eventos, para construir aplicaciones escalables y mantenibles.
                 </p>
               </div>
               <div className="skill-card" data-aos="flip-left" data-aos-delay="300">
                 <img alt="express" className="skills-img" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" title="jQuery" />
                 <h2 className="skill-name">Express</h2>
                 <p className="skill-info">
+                  Poseo un amplio conocimiento en el uso de Express para construir APIs RESTful y aplicaciones web. He gestionado rutas dinámicas y estáticas, implementado middleware para la autenticación y validación de datos, y configurado servidores para manejar múltiples solicitudes concurrentes. Además, he trabajado con plantillas y motores de vista para generar contenido dinámico y he utilizado principios de diseño como MVC (Model-View-Controller) para estructurar el código de manera clara y organizada.
                 </p>
               </div>
 
@@ -125,6 +120,7 @@ const AboutPage = () => {
                 </svg>
                 <h2 className="skill-name">MongodDB</h2>
                 <p className="skill-info">
+                Gestiono bases de datos NoSQL con MongoDB, diseñando esquemas flexibles y realizando operaciones CRUD con Mongoose. Implemento índices, replicación y particionamiento para mejorar rendimiento y escalabilidad, y uso agregaciones para consultas complejas y análisis de datos.
                 </p>
               </div>
 
@@ -134,7 +130,18 @@ const AboutPage = () => {
                 </svg>
                 <h2 className="skill-name"> Git</h2>
                 <p className="skill-info">
+                Utilizo Git para control de versiones y colaboración en proyectos. He gestionado ramas, realizado fusiones y resuelto conflictos de código. Utilizo Git para mantener un historial claro de cambios, realizar integraciones continuas y asegurar un desarrollo organizado mediante el uso de pull requests y revisiones de código.
                 </p>
+              </div>
+              <div className="skill-card" data-aos="flip-left" data-aos-delay="300">
+                <svg className="skills-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 252">
+                  <path d="M236 194c-14 0-25 1-34 5-3 1-7 1-7 4l3 6c2 3 5 8 9 11l11 8 21 10 11 9 6 4-3-6-5-5c-5-7-11-13-18-18-6-3-18-9-20-15h-1l12-3 18-3 8-2v-2l-9-10c-8-8-18-15-28-22l-18-8c-2-1-6-2-7-4l-7-13-15-30-8-20c-18-30-38-48-68-65-6-4-14-5-22-7l-13-1-8-6C34 5 8-9 1 9c-5 11 7 22 11 28l9 13 3 9c3 8 5 17 9 24l6 10c2 2 4 3 5 6-3 4-3 9-4 13-7 20-4 44 5 59 2 4 9 14 18 10 8-3 6-13 8-22l1-4 8 14c5 9 14 18 22 24 4 3 8 8 13 10l-4-4-9-10c-8-10-14-21-20-32l-7-17-3-6c-3 4-7 7-9 12-3 7-3 17-4 26h-1c-6-1-8-7-10-12-5-12-6-32-1-46 1-4 6-15 4-19-1-3-4-5-6-7l-7-12-10-30-9-13c-3-5-7-8-10-14-1-2-2-5 0-7l2-2c2-2 9 0 11 1 6 3 12 5 17 9l8 6h4c6 1 12 0 17 2 9 3 18 7 25 12 23 14 42 35 54 59 3 4 3 8 5 12l12 26c4 8 7 16 12 23 3 4 14 6 18 8l12 4 18 12c2 2 11 7 12 10Z" fill="#00546B"/>
+                  <path d="m58 43-7 1 6 7 4 9v-1c3-1 4-4 4-8l-2-4-5-4Z" fill="#00546B"/>
+                </svg>
+                <h2 className="skill-name">SQL</h2>
+                <p className="skill-info">
+                Como programador junior, tengo experiencia en la gestión y manipulación de bases de datos relacionales utilizando SQL. Soy competente en la creación y optimización de consultas para operaciones CRUD (Create, Read, Update, Delete), el diseño de esquemas de bases de datos, y la implementación de integridad referencial mediante claves primarias y foráneas. Además, utilizo funciones agregadas, subconsultas y uniones (JOINs) para realizar análisis de datos complejos y obtener información detallada de múltiples tablas.
+                </p> 
               </div>
             </div>
           </div>
